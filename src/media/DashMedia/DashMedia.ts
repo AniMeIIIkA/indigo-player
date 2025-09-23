22aeb50b5d4fd3677e3645fb0bbd22cc0690b369
mikhail <shtanko.michael@gmail.com>
2025-09-23 18:48:01 +0300

    refactor: change shaka-player import to ES module syntax

--- src/media/DashMedia/DashMedia.ts
+++ src/media/DashMedia/DashMedia.ts
@@ -1,4 +1,4 @@
-const shaka = require('shaka-player'); // TODO: check later, because it's not a es module
+import shaka from 'shaka-player';
 import { PlayerError } from '../../PlayerError';
 import { IEventData, ITrack, Events, ITracksEventData, IAudioLanguagesEventData, ITrackChangeEventData, ErrorCodes } from '../../types';
 import { IInstance } from '../../types/IInstance';
